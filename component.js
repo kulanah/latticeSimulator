c935b5fe75241a417127ad5e6c97f46c9a4963ed
Devon <devonthebutler@gmail.com>
2017-12-20 11:58:08 -0800

    created a draw lens function for the component

--- component.js
+++ component.js
@@ -15,10 +15,13 @@ class component {
       this.oppSide = 0;
       this.xOffSet = centerPoint * 2;
     }
+
+    this.material = new THREE.MeshBasicMaterial({color: 0xff37d8})
   };
 
 
   createLens(){
+    //TODO: create lens stuff
     this.shape.moveTo(this.xOffset, this.y);
     this.shape.lineTo(this.centerPoint, this.focalLength);
     this.shape.lineTo(this.oppSide, this.focalLength * 2);
@@ -27,5 +30,12 @@ class component {
     this.shape.lineTo(this.xOffset, this.focalLength * 2);
     this.shape.lineTo(this.centerPoint, this.focalLength * 3);
     this.shape.lineTo(this.oppSide, this.focalLength * 4);
+
+    this.geometry = new THREE.ShapeGeometry(this.shape);
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+  }
+
+  drawLens(scene){
+    scene.add(this.mesh);
   }
 }
