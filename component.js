49ef1b76612da281353b9d9810210b1f0184fd9c
Devon <devonthebutler@gmail.com>
2017-12-19 22:19:40 -0800

    created base storage for lenscomponent

--- component.js
+++ component.js
@@ -1,3 +1,17 @@
 class component {
-  constructor(xOffset, y, focalLength, 
+  constructor(originSide, height, centerPoint, xOffset, y, focalLength){
+    this.originSide = originSide;
+    this.height = height;
+    this.centerPoint = centerPoint;
+    this.xOffset = xOffset;
+    this.y = y;
+    this.focalLength = focalLength;
+    this.shape = new THREE.Shape();
+  };
+
+  createLens(){
+    this.shape.moveTo()
+
+  }
+
 }
