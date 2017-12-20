bc8a31f89871234c77cbd059d19b0c7a9e147535
Devon <devonthebutler@gmail.com>
2017-12-19 22:23:05 -0800

    made xoffset set programatically

--- component.js
+++ component.js
@@ -1,17 +1,29 @@
 class component {
-  constructor(originSide, height, centerPoint, xOffset, y, focalLength){
+  constructor(originSide, height, centerPoint, y, focalLength, width){
     this.originSide = originSide;
     this.height = height;
     this.centerPoint = centerPoint;
-    this.xOffset = xOffset;
     this.y = y;
     this.focalLength = focalLength;
+    this.width = width;
+
     this.shape = new THREE.Shape();
+
+    if (this.originSide === 'left'){
+      this.xOffSet = 0;
+    } else {
+      this.xOffSet = centerPoint + width;
+    }
   };
 
+
   createLens(){
+    if(this.originSide === 'left'){
+
+    } else {
+
+    }
+
     this.shape.moveTo()
-
   }
-
 }
