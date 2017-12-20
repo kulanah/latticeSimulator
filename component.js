29533d5ca434fe55e5e19c291103b3260e765616
Devon <devonthebutler@gmail.com>
2017-12-19 22:40:58 -0800

    created code to draw basic lens rays

--- component.js
+++ component.js
@@ -1,29 +1,31 @@
 class component {
-  constructor(originSide, height, centerPoint, y, focalLength, width){
+  constructor(originSide, height, centerPoint, y, focalLength){
     this.originSide = originSide;
     this.height = height;
     this.centerPoint = centerPoint;
     this.y = y;
     this.focalLength = focalLength;
-    this.width = width;
 
     this.shape = new THREE.Shape();
 
     if (this.originSide === 'left'){
       this.xOffSet = 0;
+      this.oppSide = centerPoint * 2;
     } else {
-      this.xOffSet = centerPoint + width;
+      this.oppSide = 0;
+      this.xOffSet = centerPoint * 2;
     }
   };
 
 
   createLens(){
-    if(this.originSide === 'left'){
-
-    } else {
-
-    }
-
-    this.shape.moveTo()
+    this.shape.moveTo(this.xOffset, this.y);
+    this.shape.lineTo(this.centerPoint, this.focalLength);
+    this.shape.lineTo(this.oppSide, this.focalLength * 2);
+    this.shape.lineTo(this.oppSide, this.focalLength * 4);
+    this.shape.lineTo(this.xOffset, this.y);
+    this.shape.lineTo(this.xOffset, this.focalLength * 2);
+    this.shape.lineTo(this.centerPoint, this.focalLength * 3);
+    this.shape.lineTo(this.oppSide, this.focalLength * 4);
   }
 }
