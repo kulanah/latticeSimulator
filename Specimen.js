6e9cdaf2faed57e5fcd8f9bdbe88f2f2fa4513f9
Devon <devonthebutler@gmail.com>
2017-12-28 12:34:01 -0800

    changed object to wireframe and added animation to that

--- Specimen.js
+++ Specimen.js
@@ -10,18 +10,21 @@ class Specimen{
     }
 
     this.material = new THREE.MeshBasicMaterial ({color: 0xff37d8, wireframe: true, transparent: true})
+    this.edges = new THREE.EdgesGeometry(this.shape);
+    this.line = new THREE.LineSegments(this.edges, new THREE.LineBasicMaterial({color: 0xff37d8}));
+
 
     this.object = new THREE.Mesh(this.shape, this.material);
 
   }
   
   drawShape(scene){
-    scene.add(this.object)
+    scene.add(this.line)
   }
 
   rotateObject(){
-    this.object.rotation.x += 0.004;
-    this.object.rotation.y += 0.004;
+    this.line.rotation.x += 0.004;
+    this.line.rotation.y += 0.004;
 
   }
 }
