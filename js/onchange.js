b6737146bba25da58f26230df27c7395dbc1bf1c
Devon <devonthebutler@gmail.com>
2018-02-05 23:41:58 -0800

    set blank templates for teh onchanges

---
+++ js/onchange.js
@@ -0,0 +1,23 @@
+$('#angleA').on('change', function(){
+
+});
+
+$('#angleB').on('change', function(){
+
+});
+
+$('#angleC').on('change', function(){
+
+});
+
+$('#lengthX').on('change', function(){
+
+});
+
+$('#lengthY').on('change', function(){
+
+});
+
+$('#lengthY').on('change', function(){
+
+});
