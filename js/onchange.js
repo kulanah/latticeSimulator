1b9ba82fe66a907ec7723bcad0888b9cd2d8ec84
Devon <devonthebutler@gmail.com>
2018-02-11 17:46:53 -0800

    added onchange functions and linked them to inputs

--- js/onchange.js
+++ js/onchange.js
@@ -1,26 +1,30 @@
 $('#angleA').on('input', function(){
   newSpecimen.changeAngleA($('#angleA')[0].value);
-  console.log($('#angleA')[0].value);
+  newSpecimen.redrawCrystals();
+});
+
+$('#angleB').on('input', function(){
+  newSpecimen.changeAngleB($('#angleB')[0].value);
   newSpecimen.redrawCrystals();
 
 });
 
-$('#angleB').on('input', function(){
-
-});
-
 $('#angleC').on('input', function(){
-
+  newSpecimen.changeAngleC($('#angleC')[0].value);
+  newSpecimen.redrawCrystals();
 });
 
 $('#lengthX').on('input', function(){
-
+  newSpecimen.changeLengthX($('#lengthX')[0].value);
+  newSpecimen.redrawCrystals();
 });
 
 $('#lengthY').on('input', function(){
-
+  newSpecimen.changeLengthY($('#lengthY')[0].value);
+  newSpecimen.redrawCrystals();
 });
 
-$('#lengthY').on('input', function(){
-
+$('#lengthZ').on('input', function(){
+  newSpecimen.changeLengthZ($('#lengthZ')[0].value);
+  newSpecimen.redrawCrystals();
 });
