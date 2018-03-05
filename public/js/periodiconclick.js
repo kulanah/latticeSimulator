c9aec2fdb460c724562d66bc7947eb42724cf862
kulanah <devonthebutler@gmail.com>
2018-03-05 14:10:39 -0800

    added line breaks in the onlclick

--- public/js/periodiconclick.js
+++ public/js/periodiconclick.js
@@ -1,362 +1,479 @@
 $('#H').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#He').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Li').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Be').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#B').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#C').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#N').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#O').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#F').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ne').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Na').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Mg').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Al').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Si').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#P').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#S').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cl').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ar').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#K').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ca').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Sc').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ti').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#V').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cr').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Mn').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Fe').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Co').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ni').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cu').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Zn').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ga').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ge').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#As').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Se').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Br').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Kr').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Rb').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Sr').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Y').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Zr').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Nb').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Mo').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Tc').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ru').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Rh').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Pd').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ag').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cd').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#In').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Sn').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Sb').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Te').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#I').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Xe').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cs').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ba').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Hf').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ta').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#W').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Re').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Os').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ir').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Pt').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Au').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Hg').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Tl').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Pb').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Bi').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Po').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ao').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Rn').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Fr').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ra').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Rf').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Db').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Sg').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Bh').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Hs').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Mt').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ds').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Rg').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cn').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Nh').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Fl').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Mc').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Lv').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ts').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Og').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#La').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ce').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Pr').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Nd').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Pm').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Sm').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Eu').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Gd').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Tb').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Dy').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ho').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Er').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Tm').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Yb').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Lu').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Ac').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Th').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Pa').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#U').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Np').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Pu').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Am').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cm').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Bk').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Cf').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Es').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Fm').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Md').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#No').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#Lr').on('click', function(){
   $('#elementinput').text(this.id);
 });
+
 $('#At').on('click', function(){
   $('#elementinput').text(this.id);
 });
 
 $('.element').on('click', function(){
   $('#periodictable').hide();
-
 });
