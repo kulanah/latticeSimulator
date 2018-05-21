980d72397289f1ca6a5f96c2be529457a9dfb6b0
kulanah <devonthebutler@gmail.com>
2018-05-21 14:50:34 -0700

    pulled in new components and created window to view them in

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Atoms } from './components/Atoms';
+import { Crystal } from './components/Crystal';
+import { Tabs } from './components/Tabs';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      selectedTab: 0,
+      tabs: [<Crystal/>, <Atoms/>, '']
+    };
+
+    this.setTab = this.setTab.bind(this);
+  }
+
+  setTab(index){
+    this.setState({selectedTab: index})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id='reacttest'>
+        <Tabs setTab={this.setTab}/>
+        {this.state.tabs[this.state.selectedTab]}
       </div>
     );
   }
