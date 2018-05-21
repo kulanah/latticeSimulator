6052f9b709566071a5545ce1b8c90e101d3aae7e
kulanah <devonthebutler@gmail.com>
2018-05-21 14:49:41 -0700

    added placeholder atom component

---
+++ src/components/Atoms.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../windows.css';
+
+class Atoms extends React.Component{
+  render(){
+    return(
+      <div><h1>AHHH THIS IS ATOMS</h1></div>
+    )
+  }
+}
+export { Atoms };
